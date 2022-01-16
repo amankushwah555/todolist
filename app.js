a65a8b85758e29fa593af3c3e99ff95e5e0dbbb8
Aman Kushwah <as6093614@gmail.com>
2022-01-16 20:13:01 +0530

    Third  commit

--- app.js
+++ app.js
@@ -134,12 +134,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-  port == 8000;
-}
-app.listen(port);
-
-app.listen(port, function() {
-  console.log("Server has started Successfully");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
 });
